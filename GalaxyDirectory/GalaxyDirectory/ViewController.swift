acab25c5441f55b615e724c21c0c021ac02f9a89
Gabe Guerrero <gguerrero11@gmail.com>
2019-08-09 19:06:24 -0600

    Initial Commit

---
+++ GalaxyDirectory/GalaxyDirectory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GalaxyDirectory
+//
+//  Created by Gabe Guerrero on 8/9/19.
+//  Copyright © 2019 Gabriel Guerrero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
